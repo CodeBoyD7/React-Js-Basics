a5f7d61fbd5af2a29b20e261e21bf3c00c6a0982
deva <vasudeva2002y@gmail.com>
2025-03-04 12:16:13 +0530

    React js basics

---
+++ 01. Components/live coding/src/App.jsx
@@ -0,0 +1,18 @@
+// function App() {
+//   return <div>Component 🤝</div>;
+// }
+
+// export default App;
+// --------------------------
+
+// rafce
+const App = () => {
+  return (
+    <div>
+      <div>This is heading</div>
+      <div>This is the component</div>
+    </div>
+  );
+};
+
+export default App;
